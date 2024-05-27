dd95a38966707396c35901da8428874f2d416130
Christian Nuss <christian.nuss@gmail.com>
2024-05-27 18:57:02 -0400

    pull the right arch

--- awslambda/layer/build-and-publish.sh
+++ awslambda/layer/build-and-publish.sh
@@ -8,7 +8,7 @@ ARCH=$3
 mkdir -p /tmp/layer
 
 docker pull $IMAGE
-docker create --name extract $IMAGE
+docker create --platform $ARCH --name extract $IMAGE
 docker cp extract:/opt /tmp/layer
 docker rm -f extract
 
@@ -16,9 +16,16 @@ docker rm -f extract
 cd /tmp/layer
 zip -r /tmp/layer.zip .
 
+# Convert ARCH to x86_64 or arm64
+if [ $ARCH == "linux/arm64" ]; then
+    LAYER_ARCH="arm64"
+else
+    LAYER_ARCH="x86_64"
+fi
+
 # Publish the Lambda Layer
 aws lambda publish-layer-version \
     --layer-name $NAME \
     --zip-file fileb:///tmp/layer.zip \
     --compatible-runtimes provided.al2 provided.al2023 \
-    --compatible-architectures $ARCH
+    --compatible-architectures $LAYER_ARCH
