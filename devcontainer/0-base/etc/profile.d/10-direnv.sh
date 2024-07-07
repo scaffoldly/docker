674c894191e30edbb12b2b12aef3fc3db3ddbd3e
Christian Nuss <christian.nuss@gmail.com>
2024-07-07 18:14:39 -0400

    use bash.bashrc and zshrc

--- devcontainer/0-base/etc/profile.d/10-direnv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -n "$BASH_VERSION" ]; then
-    eval "$(direnv hook bash)"
-elif [ -n "$ZSH_VERSION" ]; then
-    eval "$(direnv hook zsh)"
-fi
-
-# TODO: Other shells (fish, etc.)
